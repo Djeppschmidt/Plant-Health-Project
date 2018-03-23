5f1164d1d692c76b8377b17d4caa4f5aeedc5891
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2018-03-23 10:52:21 -0400

    updates

--- Preprocess_Phyloseq.R
+++ Preprocess_Phyloseq.R
@@ -8,12 +8,13 @@
 #R
 getwd() #make sure in correct working directory
 
+set.seed(397652)
 library(phyloseq)
 ### CHECK FIRST IMPORT STEP!! SOMETHING WRONG WHEN MULTIPLYING!!! ### <- this is because urbana is missing sample names!! ugh!
 ##I think this ^ is resolved...
 sampleDat<-as.data.frame(read.csv("Master_SampleData_Table_PHPv3_2.csv", stringsAsFactors=FALSE))
-c.factor<-as.numeric(sampleDat$Correction)
-c.factor[c.factor==NA]<-1
+#c.factor<-as.numeric(sampleDat$Correction) # needs to happen after rarefaction
+#c.factor[c.factor==NA]<-1
 rownames(sampleDat)<-sampleDat$X.SampleID
 sampleDat<-sample_data(sampleDat)
 
@@ -34,24 +35,32 @@ ps<-merge_phyloseq(com.Dat, sampleDat, tax)
 #run this after making phyloseq object
 com.rare<-rarefy_even_depth(ps, sample.size = 20000, rngseed = FALSE, replace = FALSE, trimOTUs = TRUE, verbose = TRUE)
 
+saveRDS(com.rare, "rarefied_com.rds")
 
+
+#cd /project/php-bacarc/new_preproc/sequence_variants2/
+#com.rare<-readRDS("rarefied_com.rds")
  #name QPCR correction factor to be applied to sequence counts
-rare.OTU<-as.data.frame(as.matrix(otu_table(com.Dat))) #prepare sequence counts for transformation
+
+
+rare.OTU<-as.data.frame(as.matrix(otu_table(com.rare))) #prepare sequence counts for transformation
 rare.OTU[1:3,1:3]
 
-
+c.factor<-as.numeric(sample_data(com.rare)$Correction)
+c.factor[c.factor==NA]<-1
 #adj.comDat<-com.Dat*sampleDat$Correction #do transformation/ test case not working
 adj.rareCom<-rare.OTU*c.factor
 
 head(adj.rareCom[1:3,1:3]) #visually check transformation
 
-adj.comDat<-round(adj.comDat) #get rid of decimals/ convert to count data again
+adj.rareCom<-round(adj.rareCom) #get rid of decimals/ convert to count data again
 
-adj.comDat<-otu_table(adj.comDat, taxa_are_rows=F) #change based on output from above!!!
+adj.rareCom<-otu_table(adj.rareCom, taxa_are_rows=F) #change based on output from above!!!
 
-sampleDat<-sample_data(sampleDat)
+#sampleDat<-sample_data(sampleDat)
 
-
-PSadj.comDat<-merge_phyloseq(adj.comDat, sampleDat, tax)
+PSadj.comDat<-com.rare
+otu_table(PSadj.comDat)<-adj.rareCom
+otu_table(PSadj.comDat)[1:3,1:3]#check that OTU table was added correctly
 
 saveRDS(PSadj.comDat, "QPCR_Corrected_totalPS.rds")
