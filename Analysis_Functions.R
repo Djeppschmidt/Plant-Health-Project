73a7ec872469595df2acedf4c7cb43f5f740db8f
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2018-03-26 14:27:04 -0400

    more edits

--- Analysis_Functions.R
+++ Analysis_Functions.R
@@ -1,11 +1,13 @@
 #analysis functions
 
-
+#model<-formula(y~a*b+c+d+f)
 GenStat<-function(Input, model, Output){
+  set.seed(397652)
+  dir.create(Output)
   require(phyloseq)
   this1<-readRDS(Input) #import
   #alpha diversity
-  pdf(file=paste0(Output,DivPlot.pdf,sep="/"), append=TRUE)
+  pdf(file=paste0(Output,"DivPlot.pdf",sep=""))
 
   plot_richness(this1, x="Sampling_date", title="Sampling_date")
   plot_richness(this1, x="Glyphosphate_Treatment", title="Glyphosphate_Treatment")
@@ -22,14 +24,35 @@ GenStat<-function(Input, model, Output){
   f<-sample_data(this1)$Soil_Zone
   y<-unlist(estimate_richness(this1, split=TRUE, measures= "Observed"), recursive=T, use.names=T)
   #use factors in model
-  sink(paste0(Output,DivStats.txt,sep="/"), append=TRUE)
+  sink(paste0(Output,DivStats.txt,sep=""), append=TRUE)
 
-  
-summary(aov(model))
 
-dev.off()
+  summary(aov(model))
+  y<-phyloseq::distance(this1, method="bray")
+  sampledf<-data.frame(sample_data(this1))
+  adonis(model, data=sampledf)
+  beta<-betadisper(y, a)#test dispersions of all groups
+  permutest(beta)
+
+  beta<-betadisper(y, b)
+  permutest(beta)
+
+  beta<-betadisper(y, c)
+  permutest(beta)
+
+  beta<-betadisper(y, d)
+  permutest(beta)
+
+  beta<-betadisper(y, f)
+  permutest(beta)
+
+  dev.off()
   #PERMANOVA
   #ORDINATION
-
+  pdf(file=paste0(Output,"ordination.pdf",sep=""))
+  nmds<-ordinate(this1,method="NMDS",distance="bray")
+  plot_ordination(physeq=this1,ordination=nmds,color="System.loc", shape="Soil_Zone")
+  dev.off()
+  #add more ordination types...
 
 }
