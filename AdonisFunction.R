d7294298b0c30d315e0aa09e090a6be71f537b96
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2018-05-18 15:04:24 -0400

    working now

--- AdonisFunction.R
+++ AdonisFunction.R
@@ -6,16 +6,16 @@
 # indics<- list/vector of species of interest to look at coefficients
 
 permanova<-function(ps , n, indics) {
-out<-adonis(otu_table(ps) ~ System.loc + Glyphosphate_Treatment + Sampling_date + Glyphosphate_Treatment:Sampling_date, strata = sample_data(ps)$Loc_plot_ID, as(sample_data(ps), "data.frame"))
+out=adonis(otu_table(ps) ~ System.loc + Glyphosphate_Treatment + Sampling_date + Glyphosphate_Treatment:Sampling_date, strata = sample_data(ps)$Loc_plot_ID, as(sample_data(ps), "data.frame"))
 
 tcoeffs<-data.frame(t(out$coefficients))
 tcoeffs$ID<-rownames(tcoeffs)
 
-syst<-rbind(tcoeffs[which(tcoeffs$System.loc1> quantile(tcoeffs$System.loc1, prob=1-n/100)),], tcoeffs[which(tcoeffs$System.loc1< quantile(tcoeffs$System.loc1, prob=n/100)),]) #filter taxa based on steepest n% slopes in system.loc
+syst=rbind(tcoeffs[which(tcoeffs$System.loc1> quantile(tcoeffs$System.loc1, prob=1-n/100)),], tcoeffs[which(tcoeffs$System.loc1< quantile(tcoeffs$System.loc1, prob=n/100)),]) #filter taxa based on steepest n% slopes in system.loc
   
-glyph<-rbind(tcoeffs[which(tcoeffs$Glyphosphate_Treatment1> quantile(tcoeffs$Glyphosphate_Treatment1, prob=1-n/100)),], tcoeffs[which(tcoeffs$Glyphosphate_Treatment1< quantile(tcoeffs$Glyphosphate_Treatment1, prob=n/100)),]) #filter taxa based on steepest 5% slopes in Glyphosphate Treatment
+glyph=rbind(tcoeffs[which(tcoeffs$Glyphosphate_Treatment1> quantile(tcoeffs$Glyphosphate_Treatment1, prob=1-n/100)),], tcoeffs[which(tcoeffs$Glyphosphate_Treatment1< quantile(tcoeffs$Glyphosphate_Treatment1, prob=n/100)),]) #filter taxa based on steepest 5% slopes in Glyphosphate Treatment
   
-date<-rbind(tcoeffs[which(tcoeffs$Sampling_date1> quantile(tcoeffs$Sampling_date1, prob=1-n/100)),], tcoeffs[which(tcoeffs$Sampling_date1< quantile(tcoeffs$Sampling_date1, prob=n/100)),]) #filter taxa based on steepest 5% slopes in Sampling Date
+date=rbind(tcoeffs[which(tcoeffs$Sampling_date1> quantile(tcoeffs$Sampling_date1, prob=1-n/100)),], tcoeffs[which(tcoeffs$Sampling_date1< quantile(tcoeffs$Sampling_date1, prob=n/100)),]) #filter taxa based on steepest 5% slopes in Sampling Date
 
 a<-NULL
 b<-NULL
@@ -24,22 +24,19 @@ d<-NULL
 e<-NULL
 ind<-as.data.frame(a,b,c,d,e)
 
-sppInt <- for(i in indics){
+for(i in indics){
   first<-tcoeffs[which(tcoeffs$ID==i),]
   ind<-rbind(ind, first)
-  return(ind)
 }
 
-res<-NULL
-res$out<-out
-res$syst<-syst
-res$glyph<-glyph
-res$date<-date
-res$sppInt<-sppInt
-return(res)
+return(list("out"=out, "syst"=syst, "glyph"=glyph, "date"=date, "sppInt"=ind))
+
 }
 
 # test permanova function ####
+test.P<-NULL
+test2 <- permanova(sv.RRsoy, n=5, indics)
+
 test.P <- permanova(sv.RRsoy, n=5, formula, strata, indics)
 
 # explanatory variables NOT hardcoded in... ####
@@ -107,3 +104,34 @@ form<-as.formula(System.loc + Glyphosphate_Treatment + Sampling_date + Glyphosph
 strata = sample_data(sv.RRsoy)$Loc_plot_ID
 
 test2<-permanova2(sv.RRsoy, n=5, formula=System.loc + Glyphosphate_Treatment + Sampling_date + Glyphosphate_Treatment:Sampling_date, strata, indics)
+
+
+n=5
+permanova<-function(ps , n, indics) {
+  out=adonis(otu_table(sv.RRsoy) ~ System.loc + Glyphosphate_Treatment + Sampling_date + Glyphosphate_Treatment:Sampling_date, strata = sample_data(sv.RRsoy)$Loc_plot_ID, as(sample_data(sv.RRsoy), "data.frame"))
+  
+  tcoeffs<-data.frame(t(out$coefficients))
+  tcoeffs$ID<-rownames(tcoeffs)
+  syst=rbind(tcoeffs[which(tcoeffs$System.loc1> quantile(tcoeffs$System.loc1, prob=1-n/100)),], tcoeffs[which(tcoeffs$System.loc1< quantile(tcoeffs$System.loc1, prob=n/100)),]) #filter taxa based on steepest n% slopes in system.loc
+  
+  glyph=rbind(tcoeffs[which(tcoeffs$Glyphosphate_Treatment1> quantile(tcoeffs$Glyphosphate_Treatment1, prob=1-n/100)),], tcoeffs[which(tcoeffs$Glyphosphate_Treatment1< quantile(tcoeffs$Glyphosphate_Treatment1, prob=n/100)),]) #filter taxa based on steepest 5% slopes in Glyphosphate Treatment
+  
+  date=rbind(tcoeffs[which(tcoeffs$Sampling_date1> quantile(tcoeffs$Sampling_date1, prob=1-n/100)),], tcoeffs[which(tcoeffs$Sampling_date1< quantile(tcoeffs$Sampling_date1, prob=n/100)),]) #filter taxa based on steepest 5% slopes in Sampling Date
+  
+  a<-NULL
+  b<-NULL
+  c<-NULL
+  d<-NULL
+  e<-NULL
+  ind<-as.data.frame(a,b,c,d,e)
+  
+for(i in indics){
+    first<-tcoeffs[which(tcoeffs$ID==i),]
+    ind<-rbind(ind, first)
+  }
+
+  return(list("out"=out, "syst"=syst, "glyph"=glyph, "date"=date, "sppInt"=ind))
+  
+}
+first<-tcoeffs[which(tcoeffs$ID==i),]
+agh<-list("out"=out, "syst"=syst, "glyph"=glyph, "date"=date)
