3eca17083ebe546ca39046a320d5ea86128d9f50
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2020-11-25 15:37:11 -0500

    reshuffling script
    moving merging scripts from scinet file to QC preprocessing files

--- Analysis/sc_HMSC_Bac.R
+++ Analysis/sc_HMSC_Bac.R
@@ -9,18 +9,10 @@ library(phyloseq)
 library(igraph)
 library(stringr)
 library(Model.Microbiome)
-setwd("~/Documents/GitHub/Plant-Health-Project/Analysis")
-
-# import rhizosphere data
-b.RH<-readRDS("Data/BacterialRhizospherePhyloseq18112020.rds") # check scinet for new annotation + phylogenetic tree
-fun<-readRDS("Data/PHP_Fungi_2020.rds")
-
-# subset fungi to only rhizosphere samples
-f.RH<-subset_samples(fun, Soil_Zone=="rhizosphere"&Location=="Beltsville")
 
 # To Do List:
 # return to see table mergning for the metadata     [_]
-# sort code into proper files                       [_]
+# sort code into proper files                       [X]
 # Q1 Probit model                                   [X]
 # Q1 Lognormal Model                                [X]
 # Q1 (For Q2) Lognormal Model - Farming System      [X]
@@ -38,89 +30,7 @@ f.RH<-subset_samples(fun, Soil_Zone=="rhizosphere"&Location=="Beltsville")
 
 # Final Syntax Check                                [_]
 
-# reconcile fungal and bacterial sample abundance
-    # create unique sample ID to match bac to fungal samples ...
-# check that plates match
-p_table<-data.frame("plate.16s"=sample_data(b.RH)$X16s_seq_plate, "plate.ITS"= sample_data(b.RH)$ITS_seq_plate, "M.Plates"=sample_data(b.RH)$ITS_seq_plate)
-p_table$M.Plates<-as.character(p_table$M.Plates)
-p_table$M.Plates[p_table$M.Plates=="228"]<-"P1"
-p_table$M.Plates[p_table$M.Plates=="230"]<-"P2"
-p_table$M.Plates[p_table$M.Plates=="233"]<-"P3"
-p_table$M.Plates[p_table$M.Plates=="234"]<-"P4"
-p_table$M.Plates[p_table$M.Plates=="258"]<-"P5"
-p_table$M.Plates[p_table$M.Plates=="262"]<-"P6"
-p_table$M.Plates[p_table$M.Plates=="263"]<-"P7"
-p_table$M.Plates[p_table$M.Plates=="265"]<-"P8"
-#View(p_table) # names of plates are aligned
-#str_match(sample_names(b.RH), pattern="S..?")
 
-    # measure fungal to bacterial proportion by NLFA
-sample_data(b.RH)$NLFA.Ratio<-sample_data(b.RH)$NLFA.Fungi/(sample_data(b.RH)$NLFA.Gram.Positive+sample_data(b.RH)$NLFA.Gram.Negative)
-    # measure fungal to bacterial proportion by PLFA
-sample_data(b.RH)$PLFA.Ratio<-sample_data(b.RH)$PLFA.Fungi/(sample_data(b.RH)$PLFA.Gram.Positive+sample_data(b.RH)$PLFA.Gram.Negative)
-    # define total counts per sample (as a function of anchor value * relative total PLFA)
-hist(sample_sums(subset_taxa(b.RH, Kingdom=="Bacteria"))/(100*(sample_data(b.RH)$NLFA.Gram.Positive+sample_data(b.RH)$NLFA.Gram.Negative)), breaks=30, main="bacterial sequence counts per unit NLFA", xlab="Sequence counts per unit NLFA")
-
-# get total fungal NLFA into fungi ... (including merge step)
-sample_names(b.RH)<-paste(as.character(sample_data(b.RH)$ITS_seq_plate), str_match(sample_names(b.RH), pattern="S..?"), sep=".")
-fb.RH<-merge_phyloseq(b.RH, f.RH)
-fb.RH
-
-f2.RH<-subset_taxa(fb.RH, Kingdom=="k__Fungi")
-hist(sample_sums(f2.RH)/(sample_data(f2.RH)$NLFA.Fungi*100), breaks=30, main="fungal sequence counts per unit NLFA", xlab="Sequence counts per unit NLFA")
-
-    # allocate proportion of counts to fungi and bacteria
-
-trfm<-function(ps, val){
-  scaled<-data.frame(mapply(`*`, data.frame(t(as.matrix(otu_table(transform_sample_counts(ps, function(x) x/sum(x)))))), val))# sample_data(ps)$val))
-  
-  rnames<-rownames(data.frame(t(as.matrix(otu_table(ps)))))
-  cnames<-sample_names(ps)
-  rownames(scaled)<-rnames
-  colnames(scaled)<-cnames
-  scaled<-round(scaled)
-  
-  p2<-ps
-  otu_table(p2)<- otu_table(scaled, taxa_are_rows=T)
-  p2
-} # this function takes total bacteria as scaling factor, and assigns counts. Count values of less than 0.5 are rounded to 0; this way taxa that are relatively over sampled are removed from the dataset
-
-# scratch space for transforming bacteria by NLFA
-#df.s<-data.frame(mapply(`*`, data.frame(t(as.matrix(otu_table(transform_sample_counts(b.RH, function(x) x/sum(x)))))), sample_data(b.RH)$PLFA.Gram.Positive+sample_data(b.RH)$PLFA.Gram.Negative))
-
-#dim(df.s)
-#ns<-rownames(data.frame(t(as.matrix(otu_table(b.RH)))))
-#rownames(df.s)<-ns
-#b2.RH<-b.RH
-#colnames(df.s)<-sample_names(b2.RH) # make sure order is right (it is)
-#otu_table(b2.RH)<- otu_table(df.s, taxa_are_rows=T)
-#sample_names(b2.RH)
-
-b2.RH<-trfm(b.RH, 100*(sample_data(b.RH)$NLFA.Gram.Negative+sample_data(b.RH)$NLFA.Gram.Positive))
-b2.RH<-prune_taxa(taxa_sums(b2.RH) > 0, b2.RH)    
-
-f2.RH<-trfm(f2.RH, 100*sample_data(f2.RH)$NLFA.Fungi)
-f2.RH<-prune_taxa(taxa_sums(f2.RH)>0, f2.RH)
-
-
-    # merge OTU tables with names & make final phyloseq object for analysis
-
-fb.RH<-merge_phyloseq(b.RH, f2.RH)
-fb.RH
-   
-saveRDS(fb.RH, "Data/fb_Rhizosphere_2020.RDS")
-
-# scratch space for species accumulation
-sample_data(b2.RH)$NLFA.bac.tot<-sample_data(b2.RH)$NLFA.Gram.Negative+sample_data(b2.RH)$NLFA.Gram.Positive
-acum.test<-subset_samples(b2.RH, Location=="Beltsville" & NLFA.bac.tot > 19000 & NLFA.bac.tot<21000)
-acum.test<-subset_taxa(acum.test, taxa_sums(acum.test)>0)
-acum.test<-tax_glom(acum.test, taxrank="Genus")
-acum.test
-
-plot(estimate_richness(acum.test, measures="Observed")$Observed ~ sample_sums(acum.test), col=as.factor(sample_data(acum.test)$System.loc))
-
-out<-specpool(as.data.frame(as.matrix(otu_table(acum.test))))
-rarecurve()
 
 # Analysis 1: Role of AMF abundance on microbial networks; Drivers of +/- interaciton networks ; Exploratory analysis of bacterial and fungal competition or symbiosis
     # use combined dataset
